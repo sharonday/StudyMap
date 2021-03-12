93a3cd52848c40b3c1a61a832cc0216d68601828
Alyssa <alj81@pitt.edu>
2021-03-12 19:05:21 +0000

    testing out database

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from google.cloud import datastore
 
 app = Flask(__name__)
 
+datastore_client = datastore.Client("studymap-307201")
 
 @app.route("/")
 def home():
     print("Home")
+    user_key = datastore_client.key("Login", "test3")
+    user = datastore.Entity(key=user_key)
+    user["username"] = "test3"
+    user["password"] = "pass"
+    datastore_client.put(user)
+    #query = datastore_client.query(kind="Login")
+    #users = query.fetch()
+    #print(u["username"] for u in users if "username" in u)
+    print("Done")
     return render_template("index.html")
 
 
