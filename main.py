bbb2af11b4e1af713d6175dde8a0975e83ef9a76
Alyssa <alj81@pitt.edu>
2021-03-13 00:34:00 +0000

    login database and functions

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
 from google.cloud import datastore
 
+from user import UserMan
+
 app = Flask(__name__)
+app.secret_key = b"20072012f35b38f51c782e21b478395891bb6be23a61d70a"
 
 datastore_client = datastore.Client("studymap-307201")
 
 @app.route("/")
 def home():
     print("Home")
-    user_key = datastore_client.key("Login", "test3")
-    user = datastore.Entity(key=user_key)
-    user["username"] = "test3"
-    user["password"] = "pass"
-    datastore_client.put(user)
-    #query = datastore_client.query(kind="Login")
-    #users = query.fetch()
-    #print(u["username"] for u in users if "username" in u)
-    print("Done")
-    return render_template("index.html")
+    current_user = get_current_user()
+    print(current_user)
+    return render_template("index.html", user=current_user)
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html", error=[])
+
+@app.route("/signup")
+def signup_page():
+    return render_template("signup.html", error=[])
+
+# Gets the username of the current user if they are signed in
+def get_current_user():
+    return session.get("user", None)
+
+@app.route("/login-user/", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    login_info = UserMan(username, password)
+    user = login_info.verify_password(datastore_client)
+    if not user:
+        return render_template("login.html", error="Wrong Password.")
+    session["user"]=login_info.username
+    return redirect("/")
+
+@app.route("/create-user/", methods=["POST"])
+def create_new_user():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username in existing_user():
+        return render_template("signup.html", error="Username is already taken.")
+    new_user = UserMan(username, password)
+    new_user.store_user(datastore_client)
+    session["user"]=new_user.username
+    return redirect("/")    
+
+def existing_user():
+    query = datastore_client.query(kind="Login")
+    users = query.fetch()
+    return [u["username"] for u in users if "username" in u]
+
+@app.route("/logout-user/")
+def logout():
+    session.clear()
+    return("/")
 
 
 if __name__ == "__main__":
---
+++ user.py
@@ -0,0 +1,25 @@
+from google.cloud import datastore
+
+class UserMan:
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def verify_password(self, datastore_client):
+        self.datastore_client = datastore_client
+        user_key = self.datastore_client.key("Login", self.username)
+        user = self.datastore_client.get(user_key)
+        if not user:
+            return None
+        if self.password != user["password"]:
+            return None
+        return user
+
+    def store_user(self, datastore_client):
+        self.datastore_client = datastore_client
+        user_key = self.datastore_client.key("Login", self.username)
+        user = datastore.Entity(key=user_key)
+        user["username"] = self.username
+        user["password"] = self.password
+        self.datastore_client.put(user)
