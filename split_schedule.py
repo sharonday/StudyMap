774007bc3e74c96109ce1a7f898a0c62918cd0ad
Nayana Suvarna <nls5260@gmail.com>
2021-04-14 05:48:00 +0000

    added some pseudocode for rudimentary logic

--- split_schedule.py
+++ split_schedule.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class AssignmentSplitter(object):
     def __init__(self, assignments):
         self.assignments = assignments
+        self.day_dict = dict()
         self.split_assignments()
 
     def split_assignments(self):
@@ -12,6 +13,23 @@ class AssignmentSplitter(object):
         #TODO: schedule information
         for assignment in assignments:
             name, course, curr_date, due_date, hours, user = assignment
+            #determine number of days in between current and due date
+            #determine how many off days exist in between current and due date
+            #determine how many actual days exist between the current date and due date
+            #determiner number of hours per day
+            #determine if any days have a greater number of study hours compared to free time
+                # raise warning
+            #add hours per day to dictionary
+            #iterate to next assignment
+        #repeat process for each assignment
+        #return the dictionary for dates and study hours per day
+
+
+    # Draft time
+    # def getTimeBlocks(self, num_hours, num_days):
+    #     num_hours_per_day = ceil(float(num_hours) / float(num_days))
+    #     num_hours_per_day = round(x*2)/2
+    #     return num_hours_per_day
             
 
 
