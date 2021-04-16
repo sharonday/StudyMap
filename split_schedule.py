2337c931d3935a15bc8474e8bf07f06afc2e0919
Nayana Suvarna <nls5260@gmail.com>
2021-04-16 02:46:07 +0000

    added base functionality for splitting assignments for ideal situation

--- split_schedule.py
+++ split_schedule.py
@@ -2,11 +2,15 @@
 import numpy as np 
 from datetime import datetime, date, timedelta
 import pandas as pd
+from pytz import timezone
+import math
+
+eastern = timezone('US/Eastern')
 
 class AssignmentSplitter(object):
     def __init__(self, assignments, off_days, free_hours):
         self.assignments = assignments
-        self.day_dict = dict()
+        self.day_dict = dict() #key: date value: number hours allocated
         self.off_days = self.get_off_days(off_days)
         self.free_hours = self.get_free_hours(free_hours)
     
@@ -47,35 +51,61 @@ class AssignmentSplitter(object):
                 num_free_hours[5] = len(d["hours"])
             if d["day"] == "SAT":
                 num_free_hours[6] = len(d["hours"])
-        print(num_free_hours)
+        print("NUM FREE HOURS: " + str(num_free_hours))
         return num_free_hours
     
     def split_assignments(self):
         assignments =  np.array([[x['name'], x['course'], x['current_date'], x['due_date'], x['hours'], x['user']] for x in self.assignments])
         for assignment in assignments:
             name, course, curr_date, due_date, hours, user = assignment
-            #convert due date to NanoDateTime
-            #nano takes day, month, year so have to split from year, month, day
+            curr_date = curr_date.astimezone(eastern)
             x = str(due_date).split("-")
             full = x[2] + "." + x[1] + "."+ x[0]
-            full+= ' 11:59:59,76'
-            nano = datetime.strptime(full, '%d.%m.%Y %H:%M:%S,%f')
-            #determine number of days in between current and due date
-            num_days = nano.replace(tzinfo=None)-curr_date.replace(tzinfo=None)
-            #determine dates in between current and due date
+            full+= ' 11:0:0,0'
+            due_date = datetime.strptime(full, '%d.%m.%Y %H:%M:%S,%f')
+            num_days = due_date.replace(tzinfo=None)-curr_date.replace(tzinfo=None)
             between_days = self.get_between_days(curr_date, num_days)
-            #determine any off days and remove from the list of dates
             between_days = self.remove_off_days(between_days)
-            #split the assignments (return a dictionary that maps (date) --> (hours))
-            #self.get_time_per_day(between_days)
-
+            blocks_per_day = self.get_time_per_day(between_days, hours)            
+            self.split_blocks_per_day(between_days, blocks_per_day, float(hours))
+            print(self.day_dict)
+    
+    def split_blocks_per_day(self, between_days, blocks_per_day, hours):
+        total_hours = 0
+        for date in between_days:
+            #divide by two for number of hours
+            num_hours = float(blocks_per_day) / 2
+            if date.date() in self.day_dict:
+                #get current number of allocated hours
+                current_hours = sum(self.day_dict[date.date()])
+                #determine max hours for that day
+                max_hours = self.get_available_hours(date)
+                #check to see that current hours and new hours don't exceed max hours
+                if current_hours + num_hours < max_hours:
+                    #append to dictionary
+                    self.day_dict[date.date()].append(num_hours)
+                    #add current hours to running sum
+                    total_hours += num_hours
+                    if total_hours >= hours:
+                        break
+                else:
+                    continue
+            else:
+                #insert into dictionary
+                self.day_dict[date.date()] = [num_hours]
+                #add current hours to running sum
+                total_hours += num_hours
+                #determine if we've reached the allocated hours
+                if total_hours >= hours:
+                    break
+    
     def get_between_days(self, start_date, delta_days):
         between_days = []
-        for i in range(delta_days.days + 1):
+        for i in range(1, delta_days.days+1):
             between_day = start_date + timedelta(days=i)
             between_days.append(between_day)
         return between_days
-
+    
     def remove_off_days(self, dates):
         temp_dates = dates.copy()
         day_enum = {"Sunday": 0,
@@ -92,24 +122,48 @@ class AssignmentSplitter(object):
                 temp_dates.remove(date)
         return temp_dates
     
-    def get_max_hours(self, date):
-        #convert the date to a day
+    def get_curr_hours(self, dates):
+        total_curr_hours = 0
+        temp_dates = dates.copy()
+        for date in temp_dates:
+            if date in self.day_dict:
+                curr_num_hours = self.day_dict[date]
+                default_free_hours = self.get_available_hours(date) #add one for including final date
+                hours_left = default_free_hours - curr_num_hours
+                total_curr_hours += hours_left
+            else:
+                default_free_hours = self.get_available_hours(date)
+                total_curr_hours += default_free_hours
+        return total_curr_hours
+
+    def get_time_per_day(self, between_days, hours):
+        hours_available = self.get_curr_hours(between_days)
+        if float(hours) < hours_available:
+            #multiply by 2 (split into 30 min chunks)
+            num_hours = float(hours) * 2 #change factor based on discretization
+            #divide by number of days (round up)
+            num_blocks_per_day = math.ceil(num_hours/len(between_days))
+        return num_blocks_per_day
+
+    def get_available_hours(self, date):
+        day_enum = {"Sunday": 0,
+                    "Monday": 1,
+                    "Tuesday": 2,
+                    "Wednesday": 3,
+                    "Thursday": 4,
+                    "Friday": 5,
+                    "Saturday": 6}
         day = date.strftime("%A")
-        #lookup value in the free hours
-        
+        day_index = day_enum[day]
+        num_free_hours = self.free_hours[day_index]
+        return num_free_hours
     
-    # def get_time_per_day(self, between_days, hours, name, course):
-    #     num_days = len(between_days)
-    #     for day in between_days:
-            #print number of hours per day
+    
 
-        #assignment
-
-    # Draft time
-    # def getTimeBlocks(self, num_hours, num_days):
-    #     num_hours_per_day = ceil(float(num_hours) / float(num_days))
-    #     num_hours_per_day = round(x*2)/2
-    #     return num_hours_per_day
             
 
 
+
+
+
+                        
